77f87b9e7d96b569a68c272a5bdcdfccd6ab580c
Aayush <ayushkarkee6@gmail.com>
2025-04-20 17:15:06 +0545

    changed schema

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 //routes
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`);
