935abc0fb0dd906873ecf0e1df1beab149e72c38
Aayush <ayushkarkee6@gmail.com>
2025-04-24 17:31:53 +0545

    created profile update and delete controller

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,20 @@
 import express from "express";
 
-import { getDermatologist, getUser } from "../controllers/UserController";
+import {
+  deleteProfile,
+  getDermatologist,
+  getUser,
+  updateProfile,
+} from "../controllers/UserController";
+import { authMiddleware } from "../middleware/authMiddleware";
+import upload from "../config/multer";
 
 const router = express.Router();
 
+router.use(authMiddleware);
+
+router.put("/profile", upload.single("image"), updateProfile);
+router.delete("/profile", deleteProfile);
 router.get("/dermotologist", getDermatologist);
 router.get("/user", getUser);
 
